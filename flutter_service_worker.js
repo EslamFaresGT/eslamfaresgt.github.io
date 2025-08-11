b935a317c7959dbcb589814ac5215b1ca4ada533
EslamFaresGT <eslamfares@gulfterminal.net>
2025-08-11 18:42:11 +0300

    v(1.6.41)

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "e176b5dc493206603f30b0b7f7018123",
-"version.json": "f51ffeba0c821dfdab27f1a2764e807e",
+const RESOURCES = {"flutter_bootstrap.js": "e70e199757f488ea0c440005615ac703",
+"version.json": "b903a6ac3e06a57ae2d113eba00cc8e0",
 "index.html": "373bcc35890474582b5cc74bffabd7ac",
 "/": "373bcc35890474582b5cc74bffabd7ac",
-"main.dart.js": "60abbc65f735934739fda4c4b0e6885d",
+"main.dart.js": "2bc4f42fcaeae616a37a49675e3aa4e7",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "6b849ba0e0ac419cd81e45847e04f079",
 "icons/Icon-192.png": "6342778cac9d847eb0e85edcebbb491c",
