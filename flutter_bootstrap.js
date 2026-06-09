2a3c73760b08ccc5052cae86801123e0971af097
EslamFares <islamfareselazb@gmail.com>
2026-06-09 16:09:53 +0300

    update: version to (1.6.70+235)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2096828657" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1563565194" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
